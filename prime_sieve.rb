bda42bbb53fde1a2e2c8be7cef2076b893faf8f6
Andrew Vos <andrew.vos@gmail.com>
2015-02-15 15:30:39 +0000

    Add a prime sieve

---
+++ prime_sieve.rb
@@ -0,0 +1,24 @@
+class PrimeSieve
+  def fixed_amount amount
+    primes = upto(amount * amount)
+    primes.take(amount)
+  end
+
+  def upto upper_bounds
+    range      = 2..upper_bounds
+    not_primes = []
+    primes     = []
+
+    range.each do |candidate|
+      next if not_primes.include? candidate
+      not_a_prime = candidate * 2
+      while not_a_prime <= range.last do
+        not_primes << not_a_prime
+        not_a_prime = not_a_prime + candidate
+      end
+      primes << candidate unless not_primes.include?(candidate)
+    end
+
+    primes
+  end
+end
---
+++ test_prime_sieve.rb
@@ -0,0 +1,21 @@
+require_relative "prime_sieve"
+
+require "minitest/autorun"
+
+class TestPrimeSieve < Minitest::Test
+  def setup
+    @sieve = PrimeSieve.new
+  end
+
+  def test_fixed_amount
+    expected_primes = [2,  3,  5,  7, 11, 13, 17, 19, 23, 29]
+    primes = @sieve.fixed_amount(10)
+    assert_equal expected_primes, primes
+  end
+
+  def test_primes_upto
+    expected_primes = [2,  3,  5,  7, 11]
+    primes = @sieve.upto(11)
+    assert_equal expected_primes, primes
+  end
+end
